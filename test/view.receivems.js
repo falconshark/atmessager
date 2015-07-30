ebbfaa9b408a542d52f6dc9cfc9654d4f58d86fe
sardo.ip <sardo.ip@nxtomo.hk>
2015-07-30 17:16:27 +0800

    Ready for testing receivems view

---
+++ test/view.receivems.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var request = require('supertest');
+var logger = require('log4js').getLogger('Unit-Test');
+
+
+exports['Test recevie message'] = function(test){
+
+	
+}
