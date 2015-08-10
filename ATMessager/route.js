5d995aec56cfaf733b258734e90826a03a6f3bfe
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-10 18:01:47 +0800

    require health.js and message.js

--- ATMessager/route.js
+++ ATMessager/route.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var router = express.Router();
-var health = require('./health');
+var health = require('./health.js');
+var message = require('./message.js');
 
 var log4js = require('log4js');
 var logger = log4js.getLogger('Routes');
+
+var router = express.Router();
 
 router.get('/',health.healthCheck);
 router.post('/message',message.sendMessage);
