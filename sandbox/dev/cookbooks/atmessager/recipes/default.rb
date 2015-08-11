dcb46a758c51864c63c6eea91afba0d9372299ce
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-11 16:15:02 +0800

    Will only install node.js with special version

--- sandbox/dev/cookbooks/atmessager/recipes/default.rb
+++ sandbox/dev/cookbooks/atmessager/recipes/default.rb
@@ -1,12 +1,14 @@
-bash 'Add chris-lea apt-repository' do
-  user 'root'
-  code <<-EOC
-    sudo add-apt-repository -y ppa:chris-lea/node.js
-    sudo apt-get update
-  EOC
+remote_file '/tmp/nodejs.deb' do
+    source "http://ppa.launchpad.net/chris-lea/node.js/ubuntu/pool/main/n/nodejs/nodejs_0.10.37-1chl1~trusty1_amd64.deb"
+    checksum "507bd0f6c59e0652e609f72ca09abb984fdf3510d126aead78556be934577a47"
+    notifies :run, "execute[run_install-nodejs]", :immediately
 end
 
-package 'nodejs'
+execute 'run_install-nodejs' do
+    command "dpkg -i /tmp/nodejs.deb || apt-get -f install -y"
+    user "root"
+    action :nothing
+end
 
 bash 'Start ATMessager' do
   user 'vagrant'
