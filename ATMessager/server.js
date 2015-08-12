cb0e37ec1141b5aedcaa90a2deae117b6197d4c1
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-12 15:14:13 +0800

    Add setServer function and change server setting's key to lower case

--- ATMessager/server.js
+++ ATMessager/server.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var nconf = require('nconf');
 nconf.argv()
 .env()
-.file({ file: './config.json' });
+.file({ file: './config/config.json' });
 
 //Require log4js for display running log
 var log4js = require('log4js');
@@ -10,6 +10,13 @@ var logger = log4js.getLogger('Server');
 
 function setServer(app){
 
-  var port = nconf.get('server').port;  
+  var port = nconf.get('server').port;
+  var listen = app.listen(port);
 
+  logger.info('ATMessager is now running on port '+ port + '∠( ᐛ 」∠)＿');
+
+}
+
+module.exports ={
+  setServer:setServer
 }
