43c8db11de9bdf874a47438c62e4b83c3f919936
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-12 16:01:28 +0800

    Add testutil for unit test

---
+++ ATMessager/views/common/testutil.js
@@ -0,0 +1,10 @@
+function configExpress(app){
+
+  var bodyParser = require('body-parser');
+
+  app.set('views',__dirname+'/views');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use('/', router);
+
+}
