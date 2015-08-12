9cf039bb6328dc65b17199da78bcb44416812a85
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-12 16:02:23 +0800

    Fix syntax error

--- test/view.health.js
+++ test/view.health.js
@@ -3,18 +3,18 @@
 var express = require('express');
 var supertest = require('supertest');
 var logger = require('log4js').getLogger('Unit-Test');
+var app = express();
 
 var request = supertest(app);
 
 exports['Check view health'] = function(test){
 
-	request.get('/').end(function(err,res)){
+	request.get('/').end(function(err,res){
 
   test.equal(err,null,'It should not had any error!');
 
 	test.equal(res,200,'It should return 200!');
 
 	test.done();
-
-	}
+});
 }
