53453633d0dcf6d92583937be84a162aeee422d1
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-13 18:08:02 +0800

    Will receive username variable

--- atmessager/lib/vaildMessage.js
+++ atmessager/lib/vaildMessage.js
@@ -3,11 +3,26 @@ nconf.file('bots', __dirname + '/../config/bots.json')
      .file('receviers', __dirname + '/../config/receivers.json')
      .file('senders', __dirname + '/../config/senders.json');
 
-function vaildMessage(receiver,message,botname,password,testMode){
+function vaildMessage(receiver,message,botname,username,password){
 
   if(message === undefined){
 
     return {error:369};
+  }
+
+  if(nconf.get(receiver) === undefined){
+
+    return {error:333};
+  }
+
+  if(nconf.get(botname) === undefined){
+
+    return {error:360};
+  }
+
+  if(password !== nconf.get(username).password){
+
+    return {error:361};
   }
 }
 
