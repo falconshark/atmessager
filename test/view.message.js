5d575efb1a941612a5e9e6d2364529bb72a84821
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-13 11:13:44 +0800

    Fix variable name

--- test/view.message.js
+++ test/view.message.js
@@ -4,7 +4,7 @@ var express = require('express');
 var supertest = require('supertest');
 var logger = require('log4js').getLogger('Unit-Test');
 var testUtil = require('./common/testutil.js');
-var health = require('../atmessager/views/message.js');
+var message = require('../atmessager/views/message.js');
 
 var nconf = require('nconf');
 nconf.argv()
@@ -17,6 +17,8 @@ var request = supertest(app);
 
 app.get('/message',message.sendMessage);
 
+var receiver = nconf.get('message').receiver;
+
 exports['Check view health'] = function(test){
 
 	request.get('/message').end(function(err,res){
@@ -26,7 +28,6 @@ exports['Check view health'] = function(test){
 	test.equal(res.statusCode,200,'It should return 200!');
 
 	test.done();
-
 });
 }
 
@@ -34,14 +35,16 @@ exports['Test send message'] = {
 
 	'Test send message success':function(test){
 
-		request.post('/message'),
-		.send({'receiver':'','text':'Hello World'!})
-		.end(function(err,res)){
+		request.post('/message')
+		.send({'receiver':'','text':'Hello World!'})
+		.end(function(err,res){
 
 		  test.equal(err,null,'It should not had any error!')
 
 			test.equal(res.statusCode,201,'It should return 201!');
 
-		}
+			test.done();
+
+		});
 	}
 }
