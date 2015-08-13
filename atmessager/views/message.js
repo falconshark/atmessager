b986ecd527095b2050b0efd460979f23c523e2ff
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-13 15:30:58 +0800

    Change error code and fix some mistake

--- atmessager/views/message.js
+++ atmessager/views/message.js
@@ -1,19 +1,43 @@
 var request = require('request');
 var promise = require('promised-io/promise');
+var logger = require('log4js').getLogger('Unit-Test');
+var nconf = require('nconf');
+nconf.file('bots', __dirname + '/../config/bots.json')
+     .file('receviers', __dirname + '/../config/receivers.json')
+     .file('test', __dirname + '/../../test/config.json');
 
-var message = function(req,res) {
+var healthCheck = function(req,res) {
+
+    res.sendStatus(200);
+    
+};
+
+function sendMessage(req,res){
 
   var receiver = req.body.receiver;
   var message = req.body.message;
   var botname = req.body.botname;
   var password = req.body.password;
 
-};
+  var vaildResult = vaildMessage(receiver,message,botname,password);
 
-function sendMessage(receiver,message,botname,password){
+  if(vaildResult['error'] === 361){
+
+    res.sendStatus(403);
+  }
+
+}
+
+function vaildMessage(receiver,message,botname,password){
+
+  if(message === undefined){
+
+    return {error:361};
+  }
 
 }
 
 module.exports = {
+  'healthCheck':healthCheck,
   'sendMessage':sendMessage
 };
