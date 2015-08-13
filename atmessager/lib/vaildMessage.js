4330085857b0d70217a421de72eff5636aceb17a
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-13 17:52:31 +0800

    Will get config from nconf

--- atmessager/lib/vaildMessage.js
+++ atmessager/lib/vaildMessage.js
@@ -1,10 +1,14 @@
+var nconf = require('nconf');
+nconf.file('bots', __dirname + '/../config/bots.json')
+     .file('receviers', __dirname + '/../config/receivers.json')
+     .file('senders', __dirname + '/../config/senders.json');
+
 function vaildMessage(receiver,message,botname,password,testMode){
 
   if(message === undefined){
 
     return {error:369};
   }
-  
 }
 
 module.exports = {
