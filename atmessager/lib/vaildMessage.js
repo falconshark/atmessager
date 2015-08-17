a7a5b61f95d7239f9e4bd1ddd10564696dbc58b3
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-17 11:14:45 +0800

    Add comment and logger

--- atmessager/lib/vaildMessage.js
+++ atmessager/lib/vaildMessage.js
@@ -1,3 +1,5 @@
+var logger = require('log4js').getLogger('vaildMessage');
+
 var nconf = require('nconf');
 nconf.file('bots', __dirname + '/../config/bots.json')
      .file('receviers', __dirname + '/../config/receivers.json')
@@ -5,20 +7,28 @@ nconf.file('bots', __dirname + '/../config/bots.json')
 
 function vaildMessage(receiver,message,botname,username,password){
 
+  //If miss message, return error code 369
+
   if(message === undefined){
 
     return {error:369};
   }
+
+  //If receiver not found, return error code 333
 
   if(nconf.get(receiver) === undefined){
 
     return {error:333};
   }
 
+  //If bot not found, return error code 360
+
   if(nconf.get(botname) === undefined){
 
     return {error:360};
   }
+
+  //If password missmatch, return error code 361
 
   if(password !== nconf.get(username).password){
 
