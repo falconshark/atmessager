49f80a3b99185356094d1608ab29d161c749c86c
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-19 17:08:02 +0800

    Can return status code 201 after send message success

--- atmessager/views/message.js
+++ atmessager/views/message.js
@@ -1,16 +1,17 @@
 var nconf = require('nconf');
+var logger = require('log4js').getLogger('message');
 nconf.file('bots', __dirname + '/../config/bots.json')
-     .file('receviers', __dirname + '/../config/receivers.json');
+  .file('receviers', __dirname + '/../config/receivers.json');
 
 var vaildMessage = require(__dirname + '/../lib/vaildMessage').vaildMessage;
 var telegram = require(__dirname + '/../lib/telegram');
 
-var healthCheck = function(req,res) {
+var healthCheck = function(req, res) {
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 }
 
-function sendMessage(req,res){
+function sendMessage(req, res) {
 
   var receiver = req.body.receiver;
   var message = req.body.message;
@@ -18,7 +19,7 @@ function sendMessage(req,res){
   var sender = req.body.sender;
   var password = req.body.password;
 
-  var vaildResult = vaildMessage(receiver,message,botname,sender,password);
+  var vaildResult = vaildMessage(receiver, message, botname, sender, password);
 
   switch (vaildResult['error']) {
 
@@ -28,37 +29,48 @@ function sendMessage(req,res){
       res.sendStatus(400);
       break;
 
-    //Bad Request: Wrong bot name
+      //Bad Request: Wrong bot name
 
     case 360:
       res.sendStatus(401);
       break;
 
-    //Bad Request: Sender not found
+      //Bad Request: Sender not found
 
     case 380:
       res.sendStatus(406);
       break;
 
-    //Bad Request: Password not match
+      //Bad Request: Password not match
 
     case 361:
       res.sendStatus(402);
       break;
 
-    //Bad Request: Missing message
+      //Bad Request: Missing message
 
     case 369:
       res.sendStatus(403);
       break;
 
     case null:
-      telegram.sendMessage(receiver,message,botname);
-      res.sendStatus(201);
+
+      telegram.sendMessage(receiver, message, botname,function(err,message){
+
+        if(err){
+            logger.error(err);
+            res.sendStatus(407);
+            return;
+        }
+
+        logger.info(message);
+
+        res.sendStatus(201);
+    });
   }
 }
 
 module.exports = {
-  'healthCheck':healthCheck,
-  'sendMessage':sendMessage
+  'healthCheck': healthCheck,
+  'sendMessage': sendMessage
 };
