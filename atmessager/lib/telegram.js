d8641fa063dac76c97ae30b0c26e57c5415f294f
sardo.ip <sardo.ip@nxtomo.hk>
2015-08-27 14:45:32 +0800

    Will catch error when cannot get chat_id and token.

--- atmessager/lib/telegram.js
+++ atmessager/lib/telegram.js
@@ -2,56 +2,80 @@ var logger = require('log4js').getLogger('telegram');
 
 var nconf = require('nconf');
 
-if (process.env.NODE_ENV === 'unit-test'){
-    nconf.file('bots', __dirname + '/../../test/config/bots.json')
-         .file('receviers', __dirname + '/../../test/config/receivers.json')
-         .file('senders', __dirname + '/../../test/config/senders.json');
-}else{
-    nconf.file('bots', __dirname + '/../config/bots.json')
-     .file('receviers', __dirname + '/../config/receivers.json')
-     .file('senders', __dirname + '/../config/senders.json');
- }
+if (process.env.NODE_ENV === 'unit-test') {
+	nconf.file('bots', __dirname + '/../../test/config/bots.json')
+		.file('receviers', __dirname + '/../../test/config/receivers.json')
+		.file('senders', __dirname + '/../../test/config/senders.json');
+} else {
+	nconf.file('bots', __dirname + '/../config/bots.json')
+		.file('receviers', __dirname + '/../config/receivers.json')
+		.file('senders', __dirname + '/../config/senders.json');
+}
 
 var request = require('request');
 
-function sendMessage(receiver,message,botname,callback){
+function sendMessage(receiver, message, botname, callback) {
 
-  var chat_id = nconf.get(receiver).chat_id;
-  var token = nconf.get(botname).token;
+	try {
+		var chat_id = nconf.get(receiver).chat_id;
+		var token = nconf.get(botname).token;
+	} catch (ex) {
 
-  request.post({
-      url:'https://api.telegram.org/bot' + token + '/sendMessage',
-      form:{chat_id:chat_id,text:message}},
-      function(err,res,body){
+		err = {
+			description: 'chat_id or token not found'
+		};
 
-         body = JSON.parse(body);
+		logger.error(err);
 
-        if(err){
-            logger.error(err);
-            callback(err,null);
-            return;
-        }
+		callback(err);
 
-        if(body['error_code']){
+        return;
+	}
 
-            logger.error(body['description']);
+	request.post({
+			url: 'https://api.telegram.org/bot' + token + '/sendMessage',
+			form: {
+				chat_id: chat_id,
+				text: message
+			}
+		},
+		function(err, res, body) {
 
-            err = {error:body['error_code'],description:body['description']};
+			body = JSON.parse(body);
 
-            callback(err,null);
+			if (err) {
+				logger.error(err);
+				callback(err, null);
+				return;
+			}
 
-            return;
-        }
+			if (body['error_code']) {
 
-        logger.info('Message sent!');
+				logger.error(body['description']);
 
-        var successMessage = {receiver:receiver,message:message,sendTime:new Date()};
+				err = {
+					error: body['error_code'],
+					description: body['description']
+				};
 
-        callback(null,successMessage);
+				callback(err, null);
 
-    });
+				return;
+			}
+
+			logger.info('Message sent!');
+
+			var successMessage = {
+				receiver: receiver,
+				message: message,
+				sendTime: new Date()
+			};
+
+			callback(null, successMessage);
+
+		});
 }
 
 module.exports = {
-  'sendMessage':sendMessage
+	'sendMessage': sendMessage
 };
