0aa1397a9bb4fd23f1f4f35b864ee4091a480c13
Sardo <sardo.ip@sardo.work>
2015-09-05 00:32:42 +0800

    Can create access log

--- atmessager/app.js
+++ atmessager/app.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var log4js = require('log4js');
 var fs = require('fs');
 var nconf = require('nconf');
 nconf.argv()
-.env()
-.file({ file: './config/config.json' });
+  .env()
+  .file({
+    file: './config/config.json'
+  });
 
-var logfile_path = nconf.get('logfile').path;
+var access_log_path = nconf.get('access_log').path;
+var app_log_path = nconf.get('app_log').path;
+
+log4js.clearAppenders();
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file(access_log_path), 'ACCESS_LOG');
+log4js.addAppender(log4js.appenders.file(app_log_path), 'APP_LOG');
+
+var logger = log4js.getLogger('ACCESS_LOG');
+
 var app = new express();
 
 var router = require('./route.js');
 var server = require('./server.js');
 
-var env = process.env.NODE_ENV || 'development';
-if ('development' == env) {
-    app.set('views',__dirname+'/views');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use('/', router);
-}
+
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json());
+app.use(log4js.connectLogger(logger,{level:'auto'}));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use('/', router);
 
 server.startServer(app);
 module.exports = app;
