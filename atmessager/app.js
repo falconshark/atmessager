b4763ecd6413e635967635f9b3ae4c1ef5ef9754
sardo.ip <sardo.ip@nxtomo.hk>
2015-09-08 11:44:34 +0800

    Can read the config file from customized path

--- atmessager/app.js
+++ atmessager/app.js
@@ -3,10 +3,23 @@ var bodyParser = require('body-parser');
 var log4js = require('log4js');
 var fs = require('fs');
 var nconf = require('nconf');
-nconf.file('bots', __dirname + '/config/bots.json')
-.file('senders', __dirname + '/config/senders.json')
-.file('receviers', __dirname + '/config/receivers.json')
-.file('config', __dirname + '/config/config.json');
+
+var customsConfig = process.argv[2];
+var customsBots = process.argv[3];
+var customsSenders = process.argv[4];
+var customsReceivers = process.argv[5];
+
+if (!customsConfig || !customsBots || !customsSenders || !customsReceivers) {
+    nconf.file('bots', __dirname + '/config/bots.json')
+    .file('senders', __dirname + '/config/senders.json')
+	.file('receviers', __dirname + '/config/receivers.json')
+	.file('config', __dirname + '/config/config.json');
+}else{
+    nconf.file('bots', customsBots)
+    .file('senders', customsSenders)
+    .file('receviers', customsReceivers)
+    .file('config', customsConfig);
+}
 
 var access_log_path = nconf.get('access_log').path;
 var app_log_path = nconf.get('app_log').path;
@@ -26,10 +39,12 @@ var server = require('./server.js');
 
 app.set('views', __dirname + '/views');
 app.use(bodyParser.json());
-app.use(log4js.connectLogger(log4js.getLogger('ACCESS_LOG'),{level:'auto'}));
+app.use(log4js.connectLogger(log4js.getLogger('ACCESS_LOG'), {
+	level: 'auto'
+}));
 
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 app.use('/', router);
 
